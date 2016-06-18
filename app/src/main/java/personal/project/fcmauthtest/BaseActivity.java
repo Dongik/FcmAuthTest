af1795a8f879d78484729a72922b013c6946a071
kinetik.lee <dongik3475@gmail.com>
2016-06-18 18:21:49 +0900

    Initial commit

---
+++ app/src/main/java/personal/project/fcmauthtest/BaseActivity.java
@@ -0,0 +1,37 @@
+package personal.project.fcmauthtest;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HAB on 2016-06-13.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    private ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    //git test
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        hideProgressDialog();
+    }
+
+}
